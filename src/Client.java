76be2af5dca4796dbe93953f80817c6e50539d87
Dalya <dalia.alsuwayt@students.mq.edu.au>
2022-05-30 03:27:59 +1000

    Added comments and organised code

--- src/Client.java
+++ src/Client.java
@@ -2,8 +2,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Client {
     private static Socket socket;
@@ -12,6 +14,7 @@ public class Client {
 
     public void setupConnection() {
         try {
+            // Initalisation
             System.out.println("connecting to server...");
             socket = new Socket("localhost", 50000);
 
@@ -26,12 +29,13 @@ public class Client {
     }
 
     public static void main(String[] args) throws Exception {
+        String[] serverDataSplit ={};
         try {
             System.out.println("Hello, starting client...");
             Client c = new Client();
 
             c.setupConnection();
-
+            // Initial handshake
             c.dos.write("HELO\n".getBytes());
             c.dos.flush();
 
@@ -39,31 +43,32 @@ public class Client {
 
             System.out.println("response from the server:7 " + response);
 
+            // Authentication
             c.dos.write("AUTH dummy_user\n".getBytes());
             c.dos.flush();
 
             response = c.dis.readLine();
 
             System.out.println("response from the server:6 " + response);
+
+            // Send ready
+            c.dos.write("REDY\n".getBytes());
+            c.dos.flush();      
+            response = c.dis.readLine();
+            System.out.println("response from the server:1 " + response);
+
+            // Get job value
+            String newJob = String.valueOf(response);
             
-            
-            while(!(response.equals("."))){
+            while(!(newJob.equals("NONE"))){
+                // Split job details to core, memory, etc
+                String[] splitJob = newJob.split("\\s+");
+                String jobId = splitJob[2];
 
-                c.dos.write("REDY\n".getBytes());
-                c.dos.flush();      
-
-                response = c.dis.readLine();
-
-                System.out.println("response from the server:1 " + response);
-            
-                if(response.contains("JOBN")){
-
-                    String[] splitJob = response.split("\\s+");
-                    Integer numOfJObs = Integer.valueOf(splitJob[1]);
-                    System.out.println("number of jobs: " + numOfJObs);
-                    
-
-                    c.dos.write("GETS Capable 3 700 3800\n".getBytes());
+                // Handles jobs
+                if(newJob.contains("JOBN") || newJob.contains("JOBP")){
+                    // Sends GETS command 
+                    c.dos.write(("GETS Capable " + splitJob[4] + " " + splitJob[5] + " " + splitJob[6] + "\n").getBytes());
                     c.dos.flush();
 
                     response = c.dis.readLine();
@@ -71,42 +76,39 @@ public class Client {
                     System.out.println("response from the server:2 " + response);
 
                     String[] splitData = response.split("\\s+");
-                    Integer numOfServers = Integer.valueOf(splitData[1]);
+                    Integer numOfServers = Integer.parseInt(splitData[1]);
                     System.out.println("number of servers: " + numOfServers);
-                    String[] servers = new String[numOfServers];
-
+                    // Store servers in an array list
+                    List<String> servers = new ArrayList<String>();
+        
+                    // Once completed send OK
                     c.dos.write("OK\n".getBytes());
                     c.dos.flush();
 
-                    String schdeuleCommand ="";
-                    Integer max = 0;
-                    for(int i = 0; i < numOfServers; i++){
-  
+                    // Loop through all the servers and add them
+                    for (int i = 0; i < numOfServers; i++) {
                         response = c.dis.readLine();
                         System.out.println("response from the server:3 " + response);
-                        servers[i] = response;
-                        String[] server = servers[i].split("\\s+");
-                        if (Integer.valueOf(server[4])> max){
-                            schdeuleCommand = "SCHD " +server[1] + " " + server[0] + " " +server[8] + "\n";
-                            max = Integer.valueOf(server[4]);
-                        }
-
+                        //add new server to the list
+                        servers.add(response);
                     }
-                
+
                     c.dos.write("OK\n".getBytes());
                     c.dos.flush();
-                    
-                    c.dos.write(schdeuleCommand.getBytes());
-                    c.dos.flush();
-
                     response = c.dis.readLine();
-
-                    System.out.println("response from the server:6 " + response);
+                    
+                    //get the first server provided by the GETS command //change
+                    String serverData = servers.get(0);
+                    //split up the server info so its readable and easily accessable //change
+                    serverDataSplit = serverData.split("\\s+");
 
                 }
-                
+                break;
             }
-
+            
+            c.dos.write("OK\n".getBytes());
+            c.dos.flush();
+            response = c.dis.readLine();
 
             c.dos.write("QUIT\n".getBytes());
             c.dos.flush();
